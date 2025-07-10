0f10ec73ed01030df76e3121e188fa236256f000
sanjanasujeet <sanjsujeet@gmail.com>
2025-07-09 17:58:54 -0700

    trying to change the title and sidebar, not working yet

--- app.R
+++ app.R
@@ -8,10 +8,16 @@ library(shiny)
 library(bslib)
 
 # Define UI ----
-ui <- page_sidebar(
-  title = "title panel",
-  sidebar = sidebar("sidebar"),
-  "main contents"
+#ui <- page_sidebar(
+ # title = "FUERTE", sidebar = sidebar("sidebar"), "main contents")
+
+ui <- fluidPage(
+  tags$head(tags$title("FUERTE ")),  # browser tab title
+  page_sidebar(
+    title = "FUERTE",  # title in the body of page
+    sidebar = sidebar("sidebar"),
+    "Whitebark Pine"
+  )
 )
 
 # Define server logic ----
