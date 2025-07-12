8c6b96c090cc85b48ee4eb729e41541dd38553c0
sanjanasujeet <sanjsujeet@gmail.com>
2025-07-12 15:45:23 -0700

    first working basic setup

--- app.R
+++ app.R
@@ -8,17 +8,35 @@ library(shiny)
 library(bslib)
 
 # Define UI ----
-#ui <- page_sidebar(
- # title = "FUERTE", sidebar = sidebar("sidebar"), "main contents")
 
-ui <- fluidPage(
-  tags$head(tags$title("FUERTE ")),  # browser tab title
-  page_sidebar(
-    title = "FUERTE",  # title in the body of page
-    sidebar = sidebar("sidebar"),
-    "Whitebark Pine"
-  )
-)
+ui <- tags$html(
+  tags$head(
+    tags$title("FUERTE")  # tab title
+  ),
+  tags$body(
+    page_sidebar(
+      title = "FUERTE",                # body title
+      sidebar = sidebar("Whitebark Pine"),   #left sidebar 
+      #WHITEBARK
+      navset_tab(nav_panel("Intro",
+      card(
+        card_header("Whitebark Pine Background"),
+        tags$figure(card_image("www/fuerte_pine.png"), tags$figcaption(style = "font-size: 0.5em; color: #555; font-style: italic; margin-top: 0.5em;","Kat measuring a transect*replace image later")),
+        tags$p("Whitebark pine is a species of..."),
+ 
+tags$p("White Bark Pine Health
+Research Goal: Survey variability of bark beetle infestation and overall tree health at different elevations on Mammoth Mountain"),
+
+tags$p("Skills Learned: Transect setup, mapping, species identification, assessment of tree health"))),
+nav_panel("2023",
+          card(card_header("Summer 2023 Research"),
+               "Data and Researchers from the summer of 2023")),
+nav_panel("2024",
+          card(card_header("Summer 2024 Research"),
+               "Data and Researchers from the summer of 2024"))))))
+
+
+#One of our goals during our annual FUERTE field trip at SNARL is to establish long-term place-based datasets where our new fellows can learn about data collection and analysis.
 
 # Define server logic ----
 server <- function(input, output) {
